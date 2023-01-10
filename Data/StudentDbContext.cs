9c65e9dfa52e22faa653c61a65a6a6fb3fbe8952
최대식 <74227910+cds9046@users.noreply.github.com>
2023-01-10 20:55:52 +0900

    초기 커밋.

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Student { get;set; }
+        public StudentDbContext()
+        {
+
+        }
+
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasKey(e => e.Id);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = GetConnectionString("DefaultConnection");
+            optionsBuilder.UseOracle(connectionString, b => b.UseOracleSQLCompatibility("11"));
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        public string GetConnectionString(string connectionStringName)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                    .AddJsonFile("appsetting.json", optional: false, reloadOnChange: true);
+
+            var configurationRoot = builder.Build();
+
+            var connectionString = configurationRoot.GetConnectionString(connectionStringName);
+            return connectionString;
+        }
+    }
+}
